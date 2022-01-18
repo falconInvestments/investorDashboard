37d0aab096c9ec42180aa280f7e3775df6ed1f3d
Shalom Lee <shalomjlee@gmail.com>
2022-01-18 15:43:29 -0600

    changed th apostrophes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: process.env.HOST, // your endpoint
-    USER: process.env.USER, // your username
-    PASSWORD: process.env.PASSWORD, // your password
-    DB: process.ev.DATABASE, // your db name
-    dialect: postgres,
+    HOST: "process.env.HOST", // your endpoint
+    USER: "process.env.USER", // your username
+    PASSWORD: "process.env.PASSWORD", // your password
+    DB: "process.ev.DATABASE", // your db name
+    dialect: "postgres",
     }
